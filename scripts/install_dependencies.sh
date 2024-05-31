e6736304b6e100aa0c29158135c33306264ec4ed
David <dmagsipoc@ibm.com>
2024-06-01 02:10:15 +0800

    add initial files

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-npm install
+apt-get install -y curl
+
+# Install nodejs version 20
+
+## Download nodesource
+curl -fsSL https://deb.nodesource.com/setup_20.x -o nodesource_setup.sh
+## Setup node source
+bash nodesource_setup.sh
+## Install Nodejs
+apt-get install -y nodejs
+
+# Install pm2
+npm install pm2@latest express
